30898eb00296248c235d8191eeda9f5a48e9424f
suleymanyildirimm01 <suleymanyildirimmo1@gmail.com>
2026-04-28 11:04:31 +0300

    Add Google site verification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   icons: {
     icon: "https://i.hizliresim.com/1956w92.png",
   },
+  verification: {
+    google: "google764ef25bc45b1736.html",
+  },
 };
 
 export default function RootLayout({
